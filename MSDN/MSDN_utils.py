bbb2ff0bccb9f6a897fd2b2eebd8134353afd81e
tiongleslie <28769811+tiongleslie@users.noreply.github.com>
2020-05-25 10:52:49 +0900

    Update MSDN_utils.py

--- MSDN/MSDN_utils.py
+++ MSDN/MSDN_utils.py
@@ -3,6 +3,10 @@
 # Licensed under The MIT License
 # Written by CSRC, KIST
 # ---------------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import os
 import tensorflow as tf
 import tensorflow.python.util.deprecation as deprecation
